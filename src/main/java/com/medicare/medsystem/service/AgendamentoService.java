564beed946d0751dda58aabf894541569af7a3b5
Bruno-Venturini <brunofreloventurini@outlook.com>
2024-06-22 15:16:37 -0300

    fix repository

--- src/main/java/com/medicare/medsystem/service/AgendamentoService.java
+++ src/main/java/com/medicare/medsystem/service/AgendamentoService.java
@@ -2,11 +2,18 @@ package com.medicare.medsystem.service;
 
 import com.medicare.medsystem.domain.Agendamento;
 import com.medicare.medsystem.repository.IAgendamentoRepository;
+import com.medicare.medsystem.repository.IBaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgendamentoService extends BaseService<Agendamento>{
-    public AgendamentoService(IAgendamentoRepository repository) {
-        super(repository);
+    @Autowired
+    private IAgendamentoRepository repository;
+
+    @Override
+    protected IBaseRepository<Agendamento> getRepository() {
+        return repository;
     }
 }
